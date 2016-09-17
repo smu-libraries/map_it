48eaa58fa223894f020898c284cf14498d955fba
Ler Wee Hiong <hum.ler@outlook.com>
2016-09-17 13:01:31 +0800

    Revert "Add favicon."
    This reverts commit 804b4f52e29fd28211710ebd7f2ba53fd12b7706.

--- app.js
+++ app.js
@@ -15,7 +15,8 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(favicon(path.join(__dirname, 'public', 'images', 'smu_favicon.png')));
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
