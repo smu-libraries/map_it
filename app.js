8d3fbf5c87fecbea851037be1b1aece98505005e
Ler Wee Hiong <hum.ler@outlook.com>
2016-09-26 16:17:13 +0800

    Use default helmet setup

--- app.js
+++ app.js
@@ -4,9 +4,10 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+let helmet = require('helmet');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+//var routes = require('./routes/index');
+//var users = require('./routes/users');
 var map_it = require('./map_it/map_it');
 
 var app = express();
@@ -22,6 +23,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use('/map_it/public', express.static(path.join(__dirname, 'public')));
+app.use(helmet());
 
 //app.use('/', routes);
 //app.use('/users', users);
