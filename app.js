2614292fd3604f4764f63cd0ae21991756fc0fc7
Ler Wee Hiong <hum.ler@outlook.com>
2016-10-04 23:05:59 +0800

    Perform general clean up
    Remove unused routes and modules.

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 let helmet = require('helmet');
 
-//var routes = require('./routes/index');
-//var users = require('./routes/users');
-var map_it = require('./map_it/map_it');
+let map_it = require('./map_it/map_it');
 
 var app = express();
 
@@ -16,8 +13,6 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -25,8 +20,6 @@ app.use(cookieParser());
 app.use('/map_it/public', express.static(path.join(__dirname, 'public')));
 app.use(helmet());
 
-//app.use('/', routes);
-//app.use('/users', users);
 app.use('/map_it', map_it);
 
 // catch 404 and forward to error handler
