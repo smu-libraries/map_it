890fa53474ef522a78af5d6212d115bde6814aef
Ler Wee Hiong <hum.ler@outlook.com>
2017-10-23 17:23:11 +0800

    Update path to public folder

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use('/map_it/public', express.static(path.join(__dirname, 'public')));
+app.use('/public', express.static(path.join(__dirname, 'public')));
 app.use(helmet());
 app.use(helmet.contentSecurityPolicy({ directives: { defaultSrc: ["'self'"] } }));
 app.use(helmet.noCache());
